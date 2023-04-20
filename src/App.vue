1004099d9913f01de44b127bf56063e72b8aee17
Bili Jun <jun.li0220@outlook.com>
2023-04-20 19:13:08 +0800

    feat: 初始化代码仓库
    Signed-off-by: Bili Jun <jun.li0220@outlook.com>

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello Vue 2!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
